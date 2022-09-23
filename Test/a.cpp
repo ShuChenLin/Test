636edd6d6ce40ed03fe9d9c3400de03adf4ed649
ShuChenLin <ch25585.leader.tw@gmail.com>
2022-09-23 15:25:47 +0800

    ba

--- Test/a.cpp
+++ Test/a.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-#define pii pair<int, int>
+#define pii pair<signed, signed>
 #define pll pair<ll, ll>
 #define mp make_pair
 #define pb push_back
 #define fi first
 #define se second
 const signed INF = 1e9;
-const ll ll_max = (1<<30);
+const ll ll_max = (1<<60);
 
 signed main () {
     ios_base::sync_with_stdio(0); cin.tie(0);
